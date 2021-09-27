0a3b4bc867a49a70b53e91a7fb2a4bc3588a8a1c
klesio25 <klesio.k9@gmail.com>
2021-09-27 19:21:29 -0300

    Add new contents js

---
+++ Javascript ES6 essencial/Objects/1-objects.js
@@ -0,0 +1,20 @@
+let div = () => console.log("--".repeat(20));
+
+div(); //---
+("use strict ");
+
+function Animal(qtdePatas) {
+  this.qtdePatas = qtdePatas;
+}
+
+function cachorro(morde){
+  Animal.call
+}
+
+const cachorro = new Animal(8);
+
+console.log(cachorro.qtdePatas);
+console.log(cachorro instanceof Animal); //true
+console.log(cachorro instanceof Function); //false
+
+div(); //---
