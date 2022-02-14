884f3e65d034c3e44f484d53f77dafc96d61e7a1
Klesio <klesio.dev@gmail.com>
2022-02-14 00:52:38 -0300

    finished examples destructuring in js

--- Javascript-ES6-Avançado/Destructuring_ReactJS/1-Destructiong_assignment-arrays.js
+++ Javascript-ES6-Avançado/Destructuring_ReactJS/1-Destructiong_assignment-arrays.js
@@ -10,10 +10,13 @@ let [apple2, banana2, orange2, [tomato2]] = ['Apple', 'Banana', 'Orange', ['toma
 console.log(apple, apple2)
 console.log(tomato, tomato2)
 
-[apple2, banana2, orange2] = ['Apple', 'Banana']
+//[apple2, banana2, orange2] = ['Apple', 'Banana']
 
 /* Caso índice não existir teremos uma váriavel undefined diferente de um variavel multidimensional que para no nosso programa */
 console.log(orange2) //Cannot set property 'Orange' of undefined 
+
+
+// ------------------------ Exemplo de destructuring assignment
 
 let arr = ['Apple', 'orage']
 let obj = {
@@ -26,3 +29,66 @@ let {
 
 name2 = 'Herrique'
 console.log(name2)
+// ------------------------ Exemplo de destructuring assignment
+
+let listFruits = [{
+  name: 'Apple',
+  type: 'fruit'
+}]
+let obj = {
+  name: 'Celso',
+  props: {
+    age: 28,
+    favoriteColors: ['black', 'blue']
+  }
+}
+
+let age = obj.props.age;
+
+let [apple2] = listFruits;
+let {
+  props: {
+    age: age2,
+    favoriteColors: [color1, color2]
+  }
+} = obj
+
+console.log(obj)
+
+//----- Exemplo de destructuring assignment
+
+let listFruits2 = [{
+  name: 'egg',
+  type: 'food'
+}]
+
+let fruits3 = listFruits2[0].name;
+
+let [{
+  name: fruitName
+}] = listFruits2;
+
+console.log(fruitName)
+
+//----- Exemplo de destructuring assignment em funções
+
+function mult(arr) {
+  return arr[0] * arr[1]
+}
+console.log(mult([5,9]))
+
+//---Outro exemplo
+// [0, 0] valores default
+function mult([a, b] = [0, 0]) {
+  return a * b
+}
+
+console.log(mult())
+
+//---Outro exemplo
+
+function sum({a,b}) {
+  return a + b
+}
+
+console.log(sum({a:5, b:5}))
